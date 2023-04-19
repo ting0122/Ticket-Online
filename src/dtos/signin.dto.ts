88bf4b90c2957505903c73c883ea37b5c4c1313f
chen <ting0919764920@gmail.com>
2023-04-19 22:01:41 +0800

    improve login func

--- src/dtos/signin.dto.ts
+++ src/dtos/signin.dto.ts
@@ -1,6 +1,12 @@
+import { IsNotEmpty, IsString } from 'class-validator'
+
 export class SignInDto {
 
+    @IsNotEmpty()
+    @IsString()
     public readonly name: string;
     
+    @IsNotEmpty()
+    @IsString()
     public readonly password: string;
 }
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -5,6 +5,7 @@ import { User } from 'src/entities/users.entity';
 import { CreateUserDto } from 'src/dtos/create-user.dto';
 import { SignInDto } from 'src/dtos/signin.dto';
 import * as bcrypt from 'bcrypt';
+import { errorMonitor } from 'events';
 
 @Injectable()
 export class UsersService {
@@ -18,12 +19,34 @@ export class UsersService {
     }
 
     async postSignup_info(dto: CreateUserDto){
-        const encry_pwd = await bcrypt.hash(dto.password,12);
-        dto = {...dto, password:encry_pwd};
+        const { name, password, email} = dto;
+        const isExist = await this.isExistUser(name);
+        if(isExist){
+            throw new Error('Duplicate username');
+        }
+        const encry_pwd = await bcrypt.hash(password,12);
+        dto = {...dto,password : encry_pwd};
         await this.usersRepository.create(dto);
     }
 
     async postSignin_info(dto: SignInDto){
-        await this.usersRepository.findOneByOrFail({name:dto.name,password:dto.password});
+        const {name, password} = dto;
+        const user = await this.usersRepository.findOneBy({name:name});
+        if(!user){
+            throw new Error('Invalid Information');
+        }
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            throw new Error('Invalid Information');
+        }
+        return user;
+    }
+
+    private async isExistUser(username:string):Promise<boolean>{
+        const user = await this.usersRepository
+        .createQueryBuilder('User')
+        .where('User.name =: username',{username})
+        .getOne();
+        return !!user;
     }
 }
